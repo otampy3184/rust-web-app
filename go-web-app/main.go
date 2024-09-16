e5456411cd625f085716b8bc6a89bbdbb6f60968
django_freeman <o.tampodo.t.18@gmail.com>
2024-09-16 11:47:01 +0900

    main.goとパッケージフォルダ更新

--- go-web-app/main.go
+++ go-web-app/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"go-web-app/server"
+    "log"
+    "net/http"
+    "go-web-app/server"
+    "go-web-app/services"
 )
 
 func main() {
-	srv := server.NewServer()
+    err := services.InitDB()
+    if err != nil {
+        log.Fatalf("Failed to initialize DynamoDB: %v", err)
+    }
+	
+	router := server.NewRouter()
 	log.Println("Starting server on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", srv.Router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
