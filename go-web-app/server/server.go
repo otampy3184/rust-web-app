6c38886c13a297d8382bc47bbddb690bb87ab42c
django_freeman <o.tampodo.t.18@gmail.com>
2024-09-16 11:46:13 +0900

    RouterとServerを統合

--- go-web-app/server/server.go
+++ go-web-app/server/server.go
@@ -5,20 +5,14 @@ import (
     "github.com/gorilla/mux" // Routerをシンプルに管理するため、Muxを利用
 )
 
-type Server struct {
-    Router *mux.Router
-}
-
 // Serverもポインタを参照して利用する
-func NewServer() *Server {
+func NewRouter() *mux.Router {
     router := mux.NewRouter()
 
     // ルートを設定
     router.HandleFunc("/", handlers.IndexHandler).Methods("GET")
-    router.HandleFunc("/user/", handlers.UserHandler).Methods("GET")
+    router.HandleFunc("/user/{id}", handlers.GetUserHandler).Methods("GET")
+    router.HandleFunc("/user", handlers.CreateUserHandler).Methods("POST")
 
-	// Serverインスタンスを使うにあたり、参照用のServerのアドレスを返す
-    return &Server{
-        Router: router,
-    }
+    return router
 }
